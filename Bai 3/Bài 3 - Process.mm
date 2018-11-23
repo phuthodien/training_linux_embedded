2eb68860df4d85d3e0c2d31873fa20cb70dc9c8b
LuuAnPhu <luuanphu@gmail.com>
2018-11-23 14:47:56 +0700

    Upload Unit 3 p1
    Upload document for Unit 3 p1

---
+++ Bai 3/Bài 3 - Process.mm
@@ -0,0 +1,123 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1542339660418" ID="ID_200195442" MODIFIED="1542940265879" TEXT="B&#xe0;i 3 - Process">
+<node CREATED="1542941059748" ID="ID_929393784" MODIFIED="1542941092463" POSITION="right" TEXT="M&#xf4;i tr&#x1b0;&#x1edd;ng ho&#x1ea1;t &#x111;&#x1ed9;ng c&#x1ee7;a process">
+<node CREATED="1542941568579" ID="ID_213806183" MODIFIED="1542941571303" TEXT="H&#xe0;m main">
+<node CREATED="1542941572211" ID="ID_526533346" MODIFIED="1542941584335" TEXT="&#x110;i&#x1ec3;m &#x111;&#x1ea7;u ti&#xea;n &#x111;i v&#xe0;o ch&#x1b0;&#x1a1;ng tr&#xec;nh"/>
+<node CREATED="1542941589499" ID="ID_470168209" MODIFIED="1542941616024" TEXT="Prototype: int main(int argc, char *argv[])">
+<node CREATED="1542941619955" ID="ID_1255521406" MODIFIED="1542941640080" TEXT="N&#x1ebf;u ch&#xfa;ng ta khai b&#xe1;o thi&#x1ebf;u c&#xe1;c tham s&#x1ed1; th&#xec; tr&#xec;nh bi&#xea;n d&#x1ecb;ch c&#x169;ng s&#x1ebd; t&#x1ef1; &#x111;&#x1ed9;ng th&#xea;m v&#xe0;o"/>
+</node>
+<node CREATED="1542941648699" ID="ID_1256770690" MODIFIED="1542941789577" TEXT="Khi run ch&#x1b0;&#x1a1;ng tr&#xec;nh, h&#x1ec7; &#x111;i&#x1ec1;u h&#xe0;nh s&#x1ebd; ch&#x1ea1;y m&#x1ed9;t s&#x1ed1; &#x111;o&#x1ea1;n code &#x1ea9;n d&#x1ef1;a v&#xe0;o c&#xe1;c th&#xf4;ng s&#x1ed1; c&#x1ee7;a ch&#x1b0;&#x1a1;ng tr&#xec;nh, nh&#x1eef;ng &#x111;o&#x1ea1;n code &#x111;&#xf3; n&#x1eb1;m ngo&#xe0;i code c&#x1ee7;a ch&#xfa;ng ta."/>
+</node>
+<node CREATED="1542941804761" ID="ID_547662785" MODIFIED="1542942038827" TEXT="K&#x1ebf;t th&#xfa;c ch&#x1b0;&#x1a1;ng tr&#xec;nh">
+<node CREATED="1542941812659" ID="ID_1485448923" MODIFIED="1542941831991" TEXT="L&#xe0; &#x111;i&#x1ec3;m cu&#x1ed1;i c&#xf9;ng tr&#x1b0;&#x1edb;c khi &#x111;i ra kh&#x1ecf;i ch&#x1b0;&#x1a1;ng tr&#xec;nh"/>
+<node CREATED="1542941839523" ID="ID_1077574693" MODIFIED="1542941851336" TEXT="C&#xf3; nhi&#x1ec1;u c&#xe1;ch &#x111;&#x1ec3; k&#x1ebf;t th&#xfa;c ch&#x1b0;&#x1a1;ng tr&#xec;nh">
+<node CREATED="1542941852227" ID="ID_658325501" MODIFIED="1542941864351" TEXT="Ch&#x1ee7; &#x111;&#x1ed9;ng k&#x1ebf;t th&#xfa;c">
+<node CREATED="1542941864707" ID="ID_332449017" MODIFIED="1542941873104" TEXT="Call return trong h&#xe0;m main"/>
+<node CREATED="1542941873451" ID="ID_1315612743" MODIFIED="1542941893752" TEXT="Call exit() t&#x1eeb; b&#x1ea5;t c&#x1ee9; h&#xe0;m n&#xe0;o."/>
+<node CREATED="1542941897043" ID="ID_839416245" MODIFIED="1542941908527" TEXT="Call pthread_exit t&#x1eeb; last thread"/>
+</node>
+<node CREATED="1542941911578" ID="ID_522703209" MODIFIED="1542941918439" TEXT="B&#x1ecb; &#x111;&#x1ed9;ng k&#x1ebf;t th&#xfa;c">
+<node CREATED="1542941927251" ID="ID_1836100160" MODIFIED="1542941935303" TEXT="Ch&#x1b0;&#x1a1;ng tr&#xec;nh g&#x1eb7;p l&#x1ed7;i"/>
+<node CREATED="1542941936835" ID="ID_20245739" MODIFIED="1542941947119" TEXT="Nh&#x1ead;n t&#xed;n hi&#x1ec7;u k&#x1ebf;t th&#xfa;c g&#x1eed;i t&#x1eeb; b&#xea;n ngo&#xe0;i"/>
+</node>
+</node>
+<node CREATED="1542941975700" ID="ID_293294653" MODIFIED="1542942031800" TEXT="Tr&#x1b0;&#x1edb;c khi ch&#x1b0;&#x1a1;ng tr&#xec;nh th&#x1ef1;c s&#x1ef1; k&#x1ebf;t th&#xfa;c, h&#x1ec7; &#x111;i&#x1ec1;u h&#xe0;nh s&#x1ebd; gi&#x1ea3;i ph&#xf3;ng c&#xe1;c t&#xe0;i nguy&#xea;n m&#xe0; ch&#x1b0;&#x1a1;ng tr&#xec;nh &#x111;ang gi&#x1eef;, bao g&#x1ed3;m memory, file open, ..."/>
+<node CREATED="1542942153363" ID="ID_1573358064" MODIFIED="1542942180927" TEXT="Gi&#xe1; tr&#x1ecb; tr&#x1ea3; v&#x1ec1; c&#x1ee7;a ch&#x1b0;&#x1a1;ng tr&#xec;nh khi k&#x1ebf;t th&#xfa;c c&#xf3; th&#x1ec3; nh&#x1ead;n &#x111;&#x1b0;&#x1ee3;c t&#x1eeb; ch&#x1b0;&#x1a1;ng tr&#xec;nh cha.">
+<node CREATED="1542942201897" ID="ID_1090842200" MODIFIED="1542942229487" TEXT="Th&#x1ef1;c h&#xe0;nh: Xem gi&#xe1; tr&#x1ecb; tr&#x1ea3; v&#x1ec1; c&#x1ee7;a 1 command line b&#x1ea5;t k&#x1ef3;"/>
+</node>
+</node>
+<node CREATED="1542942252779" ID="ID_1719718911" MODIFIED="1542942273311" TEXT="Command line argument">
+<node CREATED="1542942279083" ID="ID_1382956377" MODIFIED="1542942306680" TEXT="Ch&#xfa;ng l&#xe0; 2 bi&#x1ebf;n argc v&#xe0; argv[] c&#x1ee7;a h&#xe0;m main"/>
+<node CREATED="1542942321539" ID="ID_1692296157" MODIFIED="1542942353391" TEXT="argc l&#xe0; s&#x1ed1; tham s&#x1ed1; truy&#x1ec1;n v&#xe0;o, argv l&#xe0; c&#xe1;c chu&#x1ed7;i k&#xfd; t&#x1ef1; truy&#x1ec1;n v&#xe0;o khi g&#x1ecd;i h&#xe0;m ch&#x1b0;&#x1a1;ng tr&#xec;nh">
+<node CREATED="1542942358531" ID="ID_76033861" MODIFIED="1542942383239" TEXT="V&#xed; d&#x1ee5;:"/>
+</node>
+</node>
+<node CREATED="1542945034393" ID="ID_1537259210" MODIFIED="1542945041245" TEXT="Environment list">
+<node CREATED="1542945043337" ID="ID_400214339" MODIFIED="1542945067021" TEXT="Store in a global variable extern char **environ"/>
+<node CREATED="1542945085696" ID="ID_1528962365" MODIFIED="1542945113461" TEXT="C&#xf3; th&#x1ec3; truy xu&#x1ea5;t th&#xf4;ng qua h&#xe0;m char *getenv(const char *name)"/>
+<node CREATED="1542945548785" ID="ID_1925232293" MODIFIED="1542945555605" TEXT="Th&#x1ef1;c h&#xe0;nh">
+<node CREATED="1542945557560" ID="ID_872421247" MODIFIED="1542945570493" TEXT="Hi&#x1ec3;n th&#x1ecb; t&#xea;n v&#xe0; gi&#xe1; tr&#x1ecb; c&#x1ee7;a c&#xe1;c bi&#x1ebf;n m&#xf4;i tr&#x1b0;&#x1edd;ng b&#x1eb1;ng C"/>
+</node>
+</node>
+<node CREATED="1542945188801" ID="ID_96523763" MODIFIED="1542945196613" TEXT="C&#x1ea5;u tr&#xfa;c b&#x1ed9; nh&#x1edb; c&#x1ee7;a process">
+<node CREATED="1542945213217" ID="ID_1740997002" MODIFIED="1542945217101" TEXT="V&#xf9;ng text">
+<node CREATED="1542945217793" ID="ID_41975710" MODIFIED="1542945225989" TEXT="N&#x1a1;i ch&#x1ee9;a m&#xe3; l&#x1ec7;nh v&#xe0; h&#x1eb1;ng s&#x1ed1;"/>
+</node>
+<node CREATED="1542945230345" ID="ID_1215708360" MODIFIED="1542945238621" TEXT="V&#xf9;ng d&#x1eef; li&#x1ec7;u &#x111;&#xe3; &#x111;&#x1b0;&#x1ee3;c kh&#x1edf;i t&#x1ea1;o"/>
+<node CREATED="1542945322937" ID="ID_845509328" MODIFIED="1542945330501" TEXT="V&#xf9;ng d&#x1eef; li&#x1ec7;u ch&#x1b0;a &#x111;&#x1b0;&#x1ee3;c kh&#x1edf;i t&#x1ea1;o"/>
+<node CREATED="1542945342673" ID="ID_307891181" MODIFIED="1542945345613" TEXT="V&#xf9;ng stack">
+<node CREATED="1542945346336" ID="ID_1871480002" MODIFIED="1542945360317" TEXT="C&#x1ea5;p ph&#xe1;t b&#x1ed9; nh&#x1edb; cho c&#xe1;c bi&#x1ebf;n khi g&#x1ecd;i h&#xe0;m"/>
+</node>
+<node CREATED="1542945363440" ID="ID_156886533" MODIFIED="1542945366949" TEXT="V&#xf9;ng heap">
+<node CREATED="1542945367808" ID="ID_1744159651" MODIFIED="1542945378413" TEXT="D&#xf9;ng &#x111;&#x1ec3; c&#x1ea5;p ph&#xe1;t b&#x1ed9; nh&#x1edb; &#x111;&#x1ed9;ng"/>
+</node>
+</node>
+<node CREATED="1542945431576" ID="ID_1045429117" MODIFIED="1542945444038" TEXT="C&#x1ea5;p ph&#xe1;t b&#x1ed9; nh&#x1edb; &#x111;&#x1ed9;ng cho process">
+<node CREATED="1542945459377" ID="ID_1573458282" MODIFIED="1542945461917" TEXT="void *malloc(size_t size);"/>
+<node CREATED="1542945470400" ID="ID_518054375" MODIFIED="1542945471108" TEXT="void *calloc(size_t nobj, size_t size);"/>
+</node>
+<node CREATED="1542945680705" ID="ID_1301859692" MODIFIED="1542945689142" TEXT="Shared library">
+<node CREATED="1542945690179" ID="ID_1482201367" MODIFIED="1542945713857" TEXT="L&#xe0; c&#xe1;c file binary &#x111;&#x1b0;&#x1ee3;c build t&#x1eeb; code C"/>
+<node CREATED="1542945714173" ID="ID_355712325" MODIFIED="1542945726930" TEXT="Ch&#xfa;ng ch&#x1ee9;a c&#xe1;c h&#xe0;m d&#xf9;ng &#x111;&#x1ec3; g&#x1ecd;i t&#x1eeb; ch&#x1b0;&#x1a1;ng tr&#xec;nh kh&#xe1;c"/>
+<node CREATED="1542945753472" ID="ID_308037813" MODIFIED="1542945815835" TEXT="M&#x1ed7;i khi c&#x1ea7;n truy xu&#x1ea5;t h&#xe0;m t&#x1eeb; th&#x1b0; vi&#x1ec7;n &#x111;&#x1ed9;ng, h&#x1ec7; &#x111;i&#x1ec1;u h&#xe0;nh s&#x1ebd; load 1 ph&#x1ea7;n ho&#x1eb7;c to&#xe0;n b&#x1ed9; th&#x1b0; vi&#x1ec7;n &#x111;&#xf3; v&#xe0;o ram, sau &#x111;&#xf3; tr&#x1ea3; v&#x1ec1; &#x111;&#x1ecb;a ch&#x1ec9; h&#xe0;m cho process. Process s&#x1ebd; g&#x1ecd;i h&#xe0;m th&#xf4;ng qua &#x111;&#x1ecb;a ch&#x1ec9;"/>
+</node>
+<node CREATED="1542951548165" ID="ID_1821200390" MODIFIED="1542951700642" TEXT="B&#xe0;i t&#x1ead;p">
+<node CREATED="1542951900165" ID="ID_393514123" MODIFIED="1542951967345" TEXT="Vi&#x1ebf;t 1 ch&#x1b0;&#x1a1;ng tr&#xec;nh C m&#x1edf; 1 file b&#x1ea5;t k&#x1ef3;, n&#x1ebf;u m&#x1edf; th&#xe0;nh c&#xf4;ng th&#xec; retrun 0 cho ch&#x1b0;&#x1a1;ng tr&#xec;nh cha, n&#x1ebf;u l&#x1ed7;i th&#xec; return -1 cho ch&#x1b0;&#x1a1;ng tr&#xec;nh cha. D&#xf9;ng terminal &#x111;&#x1ec3; ch&#x1ea1;y v&#xe0; echo v&#xe0;o bi&#x1ebf;n $ &#x111;&#x1ec3; &#x111;&#x1ecd;c k&#x1ebf;t qu&#x1ea3; tr&#x1ea3; v&#x1ec1;"/>
+<node CREATED="1542951778221" ID="ID_1224297608" MODIFIED="1542951884688" TEXT="Vi&#x1ebf;t m&#x1ed9;t ch&#x1b0;&#x1a1;ng tr&#xec;nh x&#x1eed; l&#xfd; tham s&#x1ed1; &#x111;&#x1ea7;u v&#xe0;o gi&#x1ed1;ng v&#x1edb;i c&#xe2;u l&#x1ec7;nh ls. V&#xed; d&#x1ee5; n&#x1ebf;u call &quot;my_ls -l&quot; s&#x1ebd; in ra d&#xf2;ng ch&#x1eef; hi&#x1ec7;n th&#x1ecb; full property, n&#x1ebf;u truy&#x1ec1;n -a s&#x1ebd; in hi&#x1ec3;n th&#x1ecb; file &#x1ea9;n, n&#x1ebf;u truy&#x1ec1;n -la s&#x1ebd; in ra c&#x1ea3; 2 d&#xf2;ng tr&#xea;n."/>
+<node CREATED="1542952045068" ID="ID_1866647398" MODIFIED="1542952079856" TEXT="Vi&#x1ebf;t 1 ch&#x1b0;&#x1a1;ng tr&#xec;nh C truy c&#x1ead;p v&#xe0;o environment variable v&#xe0; hi&#x1ec3;n th&#x1ecb; &#x111;&#x1b0;&#x1edd;ng d&#x1eab;n th&#x1b0; m&#x1ee5;c home, t&#xea;n user, &#x111;&#x1b0;&#x1edd;ng d&#x1eab;n hi&#x1ec7;n t&#x1ea1;i c&#x1ee7;a ch&#x1b0;&#x1a1;ng tr&#xec;nh."/>
+<node CREATED="1542952701900" ID="ID_962910778" MODIFIED="1542952817857" TEXT="Vi&#x1ebf;t 1 th&#x1b0; vi&#x1ec7;n r&#x1ed3;i build th&#xe0;nh shared library, shared lib &#x111;&#x1b0;&#x1ee3;c copy v&#xe0;o th&#x1b0; m&#x1ee5;c /usr/lib. Sau &#x111;&#xf3; vi&#x1ebf;t 1 ch&#x1b0;&#x1a1;ng tr&#xec;nh C s&#x1eed; d&#x1ee5;ng shared lib &#x111;&#xf3;."/>
+</node>
+<node CREATED="1542952237836" ID="ID_391886845" MODIFIED="1542952245971" TEXT="Terminal login">
+<node CREATED="1542952610076" ID="ID_955792440" MODIFIED="1542952640730" TEXT="M&#x1ed7;i 1 ch&#x1b0;&#x1a1;ng tr&#xec;nh khi start &#x111;&#x1ec1;u c&#x1ea7;n &#x111;&#x1b0;&#x1ee3;c g&#xe1;n c&#xe1;c lu&#x1ed3;ng STDIN, STDOUT v&#xe0; STDERR cho n&#xf3;"/>
+<node CREATED="1542952643084" ID="ID_1834644423" MODIFIED="1542952685568" TEXT="M&#x1ed9;t terminal s&#x1ebd; bao g&#x1ed3;m 3 lu&#x1ed3;ng nh&#x1ead;p xu&#x1ea5;t ti&#xea;u chu&#x1ea9;n k&#x1ec3; tr&#xea;n."/>
+<node CREATED="1542952829428" ID="ID_942251459" MODIFIED="1542952902617" TEXT="Trong ch&#x1ebf; &#x111;&#x1ed9; console, m&#x1ed7;i khi log in th&#xe0;nh c&#xf4;ng th&#xec; 1 terminal device s&#x1ebd; &#x111;&#x1b0;&#x1ee3;c t&#x1ea1;o ra, c&#xe1;c ch&#x1b0;&#x1a1;ng tr&#xec;nh &#x111;&#x1b0;&#x1ee3;c run b&#x1edf;i user &#x111;&#xf3; s&#x1ebd; g&#x1eaf;n v&#x1edb;i terminal login c&#x1ee7;a user."/>
+</node>
+<node CREATED="1542953501204" ID="ID_887868116" MODIFIED="1542953526208" TEXT="Background and force ground process">
+<node CREATED="1542953528547" ID="ID_1291474629" MODIFIED="1542953577096" TEXT="Forceground process">
+<node CREATED="1542953538132" ID="ID_572323994" MODIFIED="1542953559352" TEXT="Process &#x111;ang k&#x1ebf;t n&#x1ed1;i &#x111;&#x1ebf;n STDIN c&#x1ee7;a terminal"/>
+</node>
+<node CREATED="1542953562363" ID="ID_1643697798" MODIFIED="1542953570320" TEXT="Background process">
+<node CREATED="1542953579628" ID="ID_1344107954" MODIFIED="1542953591825" TEXT="Process ng&#x1eaf;t k&#x1ebf;t n&#x1ed1;i &#x111;&#x1ebf;n STDIN c&#x1ee7;a terminal"/>
+</node>
+</node>
+<node CREATED="1542953005556" ID="ID_1325295562" MODIFIED="1542953155107" TEXT="Process group">
+<node CREATED="1542953025595" ID="ID_1730287224" MODIFIED="1542953041224" TEXT="C&#xe1;c process l&#xe0;m chung 1 lo&#x1ea1;i c&#xf4;ng vi&#x1ec7;c c&#xf3; th&#x1ec3; &#x111;&#x1b0;&#x1ee3;c gom v&#xe0;o c&#xf9;ng 1 process group"/>
+<node CREATED="1542953747061" ID="ID_1741260486" MODIFIED="1542953792449" TEXT="D&#xf9;ng &#x111;&#x1ec3; chuy&#x1ec3;n &#x111;&#x1ed5;i force v&#xe0; background mode c&#xf9;ng v&#x1edb;i nhau"/>
+<node CREATED="1542953041651" ID="ID_1863921013" MODIFIED="1542954117561" TEXT="Process group &#x111;&#x1ea1;i di&#x1ec7;n b&#x1edf;i 1 s&#x1ed1; unsigned int">
+<node CREATED="1542953065588" ID="ID_726037610" MODIFIED="1542953075664" TEXT="pid_t getpgid(pid_t pid);"/>
+<node CREATED="1542953098251" ID="ID_1271946088" MODIFIED="1542953098937" TEXT="int setpgid(pid_t pid, pid_t pgid);"/>
+</node>
+<node CREATED="1542953103180" ID="ID_677098481" MODIFIED="1542953123509" TEXT="Khi 1 process &#x111;&#x1b0;&#x1ee3;c t&#x1ea1;o ra, m&#x1eb7;c &#x111;&#x1ecb;nh n&#xf3; s&#x1ebd; c&#xf9;ng group v&#x1edb;i process cha"/>
+<node CREATED="1542953130203" ID="ID_1837114976" MODIFIED="1542953148057" TEXT="Signal c&#xf3; th&#x1ec3; &#x111;&#x1b0;&#x1ee3;c g&#x1eed;i cho t&#x1ea5;t c&#x1ea3; c&#xe1;c process trong c&#xf9;ng 1 group"/>
+</node>
+<node CREATED="1542953184629" ID="ID_79342439" MODIFIED="1542953188625" TEXT="Sesions">
+<node CREATED="1542953205059" ID="ID_196458052" MODIFIED="1542953235991" TEXT="L&#xe0; t&#x1ead;p h&#x1ee3;p c&#x1ee7;a nhi&#x1ec1;u process group c&#xf3; chung 1 m&#xf4;i tr&#x1b0;&#x1edd;ng ho&#x1ea1;t &#x111;&#x1ed9;ng"/>
+<node CREATED="1542953799340" ID="ID_1609644942" MODIFIED="1542953808665" TEXT="C&#xf3; c&#xf9;ng li&#xea;n k&#x1ebf;t &#x111;&#x1ebf;n 1 terminal login"/>
+<node CREATED="1542953887556" ID="ID_664946108" MODIFIED="1542953912201" TEXT="Khi 1 user log out, c&#xe1;c process c&#xf3; chung session v&#x1edb;i user &#x111;&#xf3; s&#x1ebd; b&#x1ecb; exit"/>
+</node>
+<node CREATED="1542953995732" ID="ID_144836619" MODIFIED="1542953998855" TEXT="B&#xe0;i t&#x1ead;p">
+<node CREATED="1542954212942" ID="ID_1874739028" MODIFIED="1542954273265" TEXT="Vi&#x1ebf;t 3 ch&#x1b0;&#x1a1;ng tr&#xec;nh A, B, C sao cho khi &#x111;&#x1b0;&#x1ee3;c ch&#x1ea1;y ch&#xfa;ng s&#x1ebd; sleep 10s r&#x1ed3;i print process group id c&#x1ee7;a m&#xec;nh. Run A, B, C b&#x1eb1;ng c&#xe2;u l&#x1ec7;nh sau; ./A | ./B | ./C &amp; v&#xe0; xem k&#x1ebf;t qu&#x1ea3;"/>
+<node CREATED="1542954284091" ID="ID_954353603" MODIFIED="1542954334248" TEXT="Vi&#x1ebf;t 3 ch&#x1b0;&#x1a1;ng tr&#xec;nh A, B, C print session id, run ch&#xfa;ng tr&#xea;n c&#xf9;ng 1 c&#x1eed;a s&#x1ed5; terminal v&#xe0; tr&#xea;n 2 c&#x1eed;a s&#x1ed5; terminal v&#xe0; xem k&#x1ebf;t qu&#x1ea3;"/>
+<node CREATED="1542954355748" ID="ID_755347535" MODIFIED="1542954406600" TEXT="Vi&#x1ebf;t 3 ch&#x1b0;&#x1a1;ng tr&#xec;nh A, B, C &#x111;&#x1ecd;c 1 k&#xfd; t&#x1ef1; t&#x1eeb; b&#xe0;n ph&#xed;m, m&#x1edf; ch&#xfa;ng b&#x1eb1;ng 3 terminal kh&#xe1;c nhau. Vi&#x1ebf;t 1 ch&#x1b0;&#x1a1;ng tr&#xec;nh C th&#x1ee9; 4 &#x111;&#x1ec3; nh&#x1ead;p k&#xfd; t&#x1ef1; cho 3 ch&#x1b0;&#x1a1;ng tr&#xec;nh A B C."/>
+</node>
+</node>
+<node CREATED="1542941408435" ID="ID_131515611" MODIFIED="1542941413469" POSITION="left" TEXT="Gi&#x1edb;i thi&#x1ec7;u">
+<node CREATED="1542941424531" ID="ID_390681621" MODIFIED="1542941494736" TEXT="Trong b&#xe0;i n&#xe0;y ch&#xfa;ng ta s&#x1ebd; c&#xf9;ng t&#xec;m hi&#x1ec3;u v&#x1ec1; 1 kh&#xe1;i ni&#x1ec7;m c&#x1a1; b&#x1ea3;n trong h&#x1ec7; &#x111;i&#x1ec1;u h&#xe0;nh - process. Ch&#xfa;ng ta s&#x1ebd; c&#xf9;ng t&#xec;m hi&#x1ec3;u c&#xe1;ch process &#x111;&#x1b0;&#x1ee3;c sinh ra, ho&#x1ea1;t &#x111;&#x1ed9;ng v&#xe0; ch&#x1ebf;t &#x111;i nh&#x1b0; th&#x1ebf; n&#xe0;o. Ngo&#xe0;i ra ch&#xfa;ng ta s&#x1ebd; t&#xec;m hi&#x1ec3;u v&#x1ec1; m&#xf4;i tr&#x1b0;&#x1edd;ng xung quanh c&#x1ee7;a 1 process"/>
+</node>
+<node CREATED="1542941289268" ID="ID_331597873" MODIFIED="1542941291864" POSITION="left" TEXT="M&#xf4;i tr&#x1b0;&#x1edd;ng ho&#x1ea1;t &#x111;&#x1ed9;ng c&#x1ee7;a process">
+<node CREATED="1542941293035" ID="ID_1559014252" MODIFIED="1542941353805" TEXT="M&#x1ed9;t process khi ch&#x1ea1;y lu&#xf4;n c&#xf3; c&#xe1;c y&#x1ebf;u t&#x1ed1; kh&#xe1;c &#x1ea3;nh h&#x1b0;&#x1edf;ng, nh&#x1eef;ng y&#x1ebf;u t&#x1ed1; &#x111;&#xf3; &#x111;&#x1ebf;n t&#x1eeb; b&#xea;n ngo&#xe0;i kh&#xf4;ng ph&#x1ea3;i &#x111;&#x1ebf;n t&#x1eeb; source code c&#x1ee7;a ch&#xfa;ng ta. V&#xed; d&#x1ee5; nh&#x1b0; quy&#x1ec1;n c&#x1ee7;a process, c&#xe1;c tham s&#x1ed1; m&#xf4;i tr&#x1b0;&#x1edd;ng, ..."/>
+<node CREATED="1542941365937" ID="ID_1838408938" MODIFIED="1542941404677" TEXT="Trong nh&#x1eef;ng slide ti&#x1ebf;p theo ch&#xfa;ng ta s&#x1ebd; c&#xf9;ng t&#xec;m hi&#x1ec3;u v&#x1ec1; c&#xe1;c th&#xf4;ng s&#x1ed1; &#x111;&#xf3;"/>
+</node>
+<node CREATED="1542942043171" ID="ID_697111446" MODIFIED="1542942048309" POSITION="left" TEXT="K&#x1ebf;t th&#xfa;c ch&#x1b0;&#x1a1;ng tr&#xec;nh">
+<node CREATED="1542942049066" ID="ID_1125752547" MODIFIED="1542942131255" TEXT="M&#x1ed7;i khi ch&#x1b0;&#x1a1;ng tr&#xec;nh xin c&#x1ea5;p ph&#xe1;t b&#x1ea5;t c&#x1ee9; t&#xe0;i nguy&#xea;n n&#xe0;o th&#xec; h&#x1ec7; &#x111;i&#x1ec1;u h&#xe0;nh &#x111;&#x1ec1;u l&#x1b0;u th&#xf4;ng tin l&#x1ea1;i, th&#xf4;ng tin &#x111;&#xf3; &#x111;&#x1b0;&#x1ee3;c ghi trong struct process descriptor c&#x1ee7;a t&#x1eeb;ng ch&#x1b0;&#x1a1;ng tr&#xec;nh. Do &#x111;&#xf3; khi ch&#x1b0;&#x1a1;ng tr&#xec;nh k&#x1ebf;t th&#xfa;c, h&#x1ec7; &#x111;i&#x1ec1;u h&#xe0;nh c&#xf3; c&#xe1;c th&#xf4;ng tin v&#x1ec1; t&#xe0;i nguy&#xea;n m&#xe0; ch&#x1b0;&#x1a1;ng tr&#xec;nh &#x111;&#xe3; c&#x1ea5;p ph&#xe1;t, t&#x1eeb; &#x111;&#xf3; c&#xf3; th&#x1ec3; gi&#x1ea3;i ph&#xf3;ng ch&#xfa;ng."/>
+</node>
+<node CREATED="1542945495952" ID="ID_1515851909" MODIFIED="1542945506900" POSITION="left" TEXT="Environment list"/>
+<node CREATED="1542952924155" ID="ID_763511503" MODIFIED="1542952925209" POSITION="left" TEXT="Terminal login">
+<node CREATED="1542952925900" ID="ID_849781402" MODIFIED="1542952942185" TEXT="V&#xed; d&#x1ee5; cho m&#x1ecd;i ng&#x1b0;&#x1edd;i nh&#xec;n terminal trong ch&#x1ebf; &#x111;&#x1ed9; graphic"/>
+</node>
+<node CREATED="1542953605491" ID="ID_1558894357" MODIFIED="1542953606344" POSITION="left" TEXT="Background and force ground process">
+<node CREATED="1542953607259" ID="ID_1518428011" MODIFIED="1542953624584" TEXT="V&#xed; d&#x1ee5; cho m&#x1ecd;i ng&#x1b0;&#x1edd;i th&#x1ea5;y background v&#xe0; force group tr&#xea;n terminal"/>
+</node>
+</node>
+</map>
