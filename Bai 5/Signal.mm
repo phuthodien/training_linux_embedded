89b1815c84ce2a8ae0501011e4f027121a858ce2
LuuAnPhu <luuanphu@gmail.com>
2018-12-15 10:11:42 +0700

    Commit document for Unit 5
    Commit document for Unit 5

---
+++ Bai 5/Signal.mm
@@ -0,0 +1,116 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1544149943896" ID="ID_913547495" MODIFIED="1544149949684" TEXT="Signal">
+<node CREATED="1544151168588" ID="ID_1395598820" MODIFIED="1544151171136" POSITION="right" TEXT="Agenda">
+<node CREATED="1544167475180" ID="ID_1842914989" MODIFIED="1544167476049" TEXT="T&#x1ed5;ng quan v&#x1ec1; signal"/>
+<node CREATED="1544167480884" ID="ID_1031028567" MODIFIED="1544167481982" TEXT="X&#x1eed; l&#xfd; khi nh&#x1ead;n signal"/>
+<node CREATED="1544167486555" ID="ID_532169634" MODIFIED="1544167487685" TEXT="Signal table"/>
+<node CREATED="1544167496363" ID="ID_935503042" MODIFIED="1544167496913" TEXT="M&#x1ed9;t s&#x1ed1; signal c&#x1a1; b&#x1ea3;n"/>
+<node CREATED="1544167513492" ID="ID_1503403640" MODIFIED="1544167514209" TEXT="Send signal"/>
+<node CREATED="1544167531948" ID="ID_1529473089" MODIFIED="1544167534689" TEXT="Th&#x1ef1;c h&#xe0;nh"/>
+</node>
+<node CREATED="1544151926787" ID="ID_388502268" MODIFIED="1544167469671" POSITION="right" TEXT="T&#x1ed5;ng quan v&#x1ec1; signal">
+<node CREATED="1544152028691" ID="ID_1696876856" MODIFIED="1544167853895" TEXT="L&#xe0; software interrupt">
+<node CREATED="1544156011833" ID="ID_1864072591" MODIFIED="1544156025837" TEXT="Process x&#x1eed; l&#xfd; n&#xf3; gi&#x1ed1;ng nh&#x1b0; interrupt"/>
+</node>
+<node CREATED="1544152039570" ID="ID_42848359" MODIFIED="1544152056888" TEXT="L&#xe0; ph&#x1b0;&#x1a1;ng ph&#xe1;p giao ti&#x1ebf;p ph&#x1ed5; bi&#x1ebf;n nh&#x1ea5;t gi&#x1eef;a c&#xe1;c app"/>
+<node CREATED="1544152592349" ID="ID_859413559" MODIFIED="1544152618975" TEXT="Kh&#xf4;ng th&#x1ec3; l&#x1b0;&#x1edd;ng tr&#x1b0;&#x1edb;c &#x111;&#x1b0;&#x1ee3;c l&#xfa;c n&#xe0;o th&#xec; nh&#x1ead;n &#x111;&#x1b0;&#x1ee3;c signal"/>
+<node CREATED="1544152091811" ID="ID_1281669954" MODIFIED="1544152277672" TEXT="C&#xf3; 32 gi&#xe1; tr&#x1ecb; kh&#xe1;c nhau, m&#x1ed7;i gi&#xe1; tr&#x1ecb; &#x111;&#x1b0;&#x1ee3;c g&#xe1;n 1 t&#xea;n ri&#xea;ng bi&#x1ec7;t"/>
+<node CREATED="1544152416483" ID="ID_192807589" MODIFIED="1544167929015" TEXT="C&#xe1;c tr&#x1b0;&#x1edd;ng h&#x1ee3;p x&#x1ea3;y ra signal">
+<node CREATED="1544152441531" ID="ID_1655124665" MODIFIED="1544152449815" TEXT="User s&#x1eed; d&#x1ee5;ng kill command"/>
+<node CREATED="1544152450554" ID="ID_381215010" MODIFIED="1544152463407" TEXT="Process g&#x1eed;i signal"/>
+<node CREATED="1544152466067" ID="ID_1485098627" MODIFIED="1544152471575" TEXT="Ch&#x1b0;&#x1a1;ng tr&#xec;nh g&#x1eb7;p l&#x1ed7;i"/>
+<node CREATED="1544152472674" ID="ID_1095228744" MODIFIED="1544152494608" TEXT="User &#x1ea5;n 1 s&#x1ed1; t&#x1ed5; h&#x1ee3;p ph&#xed;m &#x111;&#x1eb7;c bi&#x1ec7;t"/>
+</node>
+</node>
+<node CREATED="1544152657531" ID="ID_1411667580" MODIFIED="1544153586246" POSITION="right" TEXT="X&#x1eed; l&#xfd; khi nh&#x1ead;n signal">
+<node CREATED="1544152690347" ID="ID_1493892619" MODIFIED="1544152704615" TEXT="Signal c&#xf3; th&#x1ec3; ignore"/>
+<node CREATED="1544152705659" ID="ID_635492921" MODIFIED="1544152711871" TEXT="Signal kh&#xf4;ng th&#x1ec3; ignore"/>
+<node CREATED="1544152717074" ID="ID_1473636538" MODIFIED="1544153405696" TEXT="Signal c&#xf3; th&#x1ec3; ch&#x1ee7; &#x111;&#x1ed9;ng &#x111;i&#x1ec3;u khi&#x1ec3;n &#x111;&#x1b0;&#x1ee3;c"/>
+<node CREATED="1544152727986" ID="ID_852090377" MODIFIED="1544153411479" TEXT="Signal kh&#xf4;ng th&#x1ec3; ch&#x1ee7; &#x111;&#x1ed9;ng &#x111;i&#x1ec1;u khi&#x1ec3;n &#x111;&#x1b0;&#x1ee3;c"/>
+<node CREATED="1544153649185" ID="ID_89655834" MODIFIED="1544153663846" TEXT="signal.h"/>
+<node CREATED="1544153763259" ID="ID_1425794678" MODIFIED="1544153764608" TEXT="sighandler_t signal(int signum, sighandler_t handler)"/>
+<node CREATED="1544153776061" ID="ID_1138069254" MODIFIED="1544153776625" TEXT="void sig_handler(int signo)"/>
+<node CREATED="1544153348594" ID="ID_1483974973" MODIFIED="1544153359143" TEXT="Th&#x1ef1;c h&#xe0;nh">
+<node CREATED="1544153360067" ID="ID_454098215" MODIFIED="1544153388127" TEXT="Vi&#x1ebf;t 1 ch&#x1b0;&#x1a1;ng tr&#xec;nh C &#x111;&#x1ec3; in ch&#x1eef; hello world m&#x1ed7;i khi user nh&#x1ea5;n ph&#xed;m Ctrl + C"/>
+<node CREATED="1544154811561" ID="ID_1187580114" MODIFIED="1544155897389" TEXT="Vi&#x1ebf;t 1 ch&#x1b0;&#x1a1;ng tr&#xec;nh C sleep v&#xf4; th&#x1edd;i h&#x1ea1;n, s&#x1eed; d&#x1ee5;ng 1 ch&#x1b0;&#x1a1;ng tr&#xec;nh C kh&#xe1;c g&#x1eed;i signal &#x111;&#x1ec3; wakeup ch&#x1b0;&#x1a1;ng tr&#xec;nh C &#x111;&#x1ea7;u ti&#xea;n l&#xea;n."/>
+</node>
+</node>
+<node CREATED="1544154007104" ID="ID_1258532800" MODIFIED="1544154012029" POSITION="right" TEXT="Signal table">
+<node CREATED="1544154110170" ID="ID_124699186" MODIFIED="1544154130165" TEXT="M&#x1ed7;i m&#x1ed9;t process s&#x1ebd; c&#xf3; 1 b&#x1ea3;ng signal table"/>
+<node CREATED="1544154130617" ID="ID_518062227" MODIFIED="1544154145413" TEXT="H&#x1ec7; th&#x1ed1;ng t&#x1ef1; t&#x1ea1;o ra b&#x1ea3;ng signal table khi kh&#x1edf;i t&#x1ea1;o process"/>
+<node CREATED="1544154148887" ID="ID_1310958244" MODIFIED="1544154194389" TEXT="M&#x1ed7;i &#xf4; trong b&#x1ea3;ng l&#xe0; 1 struct 2 ph&#x1ea7;n t&#x1eed;, 1 ph&#x1ea7;n t&#x1eed; l&#xe0; signal number, 1 ph&#x1ea7;n t&#x1eed; l&#xe0; &#x111;&#x1ecb;a ch&#x1ec9; c&#x1ee7;a signal handler"/>
+<node CREATED="1544154204033" ID="ID_305014538" MODIFIED="1544154239221" TEXT="Khi m&#x1edb;i &#x111;&#x1b0;&#x1ee3;c kh&#x1edf;i t&#x1ea1;o, t&#x1ea5;t c&#x1ea3; c&#xe1;c signal hander s&#x1ebd; l&#xe0; default handler"/>
+</node>
+<node CREATED="1544153478531" ID="ID_969623836" MODIFIED="1544167491449" POSITION="right" TEXT="M&#x1ed9;t s&#x1ed1; signal c&#x1a1; b&#x1ea3;n">
+<node CREATED="1544153504555" ID="ID_587017620" MODIFIED="1544153505119" TEXT="SIGCHLD"/>
+<node CREATED="1544153527435" ID="ID_1198450904" MODIFIED="1544153528224" TEXT="SIGILL"/>
+<node CREATED="1544153540557" ID="ID_681861745" MODIFIED="1544153541426" TEXT="SIGINT"/>
+<node CREATED="1544153555943" ID="ID_653917245" MODIFIED="1544153556619" TEXT="SIGKILL"/>
+<node CREATED="1544153803246" ID="ID_703580689" MODIFIED="1544153803653" TEXT="SIGSEGV"/>
+</node>
+<node CREATED="1544156402345" ID="ID_135133775" MODIFIED="1544156423413" POSITION="right" TEXT="Nh&#x1eef;ng function an to&#xe0;n khi g&#x1ecd;i trong signal handler">
+<node CREATED="1544156425723" ID="ID_1712742796" MODIFIED="1544156439626" TEXT="Signal handler &#x111;&#x1b0;&#x1ee3;c g&#x1ecd;i t&#x1ea1;i b&#x1ea5;t c&#x1ee9; th&#x1edd;i &#x111;i&#x1ec3;m n&#x1ea3;o"/>
+<node CREATED="1544156440449" ID="ID_1629199555" MODIFIED="1544156462413" TEXT="Signal handler break lu&#x1ed3;ng th&#x1ef1;c thi hi&#x1ec7;n t&#x1ea1;i c&#x1ee7;a ch&#x1b0;&#x1a1;ng tr&#xec;nh ch&#xed;nh"/>
+<node CREATED="1544156467681" ID="ID_323727787" MODIFIED="1544156480789" TEXT="C&#xf3; th&#x1ec3; g&#xe2;y xung &#x111;&#x1ed9;t d&#x1eef; li&#x1ec7;u v&#x1edb;i ch&#x1b0;&#x1a1;ng tr&#xec;nh ch&#xed;nh"/>
+<node CREATED="1544156482592" ID="ID_1994160265" MODIFIED="1544156509365" TEXT="N&#x1ebf;u x&#x1ea3;y ra bug xung &#x111;&#x1ed9;t d&#x1eef; li&#x1ec7;u trong signal handler, &#x111;&#xf3; s&#x1ebd; l&#xe0; random bug"/>
+<node CREATED="1544156514089" ID="ID_391069256" MODIFIED="1544156531349" TEXT="M&#x1ed9;t s&#x1ed1; function kh&#xf4;ng n&#xea;n s&#x1eed; d&#x1ee5;ng trong signal hanler"/>
+</node>
+<node CREATED="1544163720365" ID="ID_885364225" MODIFIED="1544163727913" POSITION="right" TEXT="Send signal">
+<node CREATED="1544163728941" ID="ID_657623710" MODIFIED="1544163740560" TEXT="#include &lt;signal.h&gt;"/>
+<node CREATED="1544163741269" ID="ID_440237907" MODIFIED="1544163746889" TEXT="int kill(pid_t pid, int signo)">
+<node CREATED="1544163793262" ID="ID_272572929" MODIFIED="1544163794721" TEXT="pid &gt; 0"/>
+<node CREATED="1544163797157" ID="ID_763026858" MODIFIED="1544163802241" TEXT="pid  ==0"/>
+<node CREATED="1544163802509" ID="ID_1330782705" MODIFIED="1544163805129" TEXT="pid &lt; 0"/>
+<node CREATED="1544163806492" ID="ID_822029605" MODIFIED="1544163814056" TEXT="pid == -1"/>
+</node>
+<node CREATED="1544163752853" ID="ID_336805726" MODIFIED="1544163753641" TEXT="int raise(int signo)"/>
+<node CREATED="1544163753956" ID="ID_1747502127" MODIFIED="1544163758145" TEXT="kill command"/>
+<node CREATED="1544163864701" ID="ID_1472096620" MODIFIED="1544163876919" TEXT="Permission to send a signal"/>
+</node>
+<node CREATED="1544152301114" ID="ID_1068322910" MODIFIED="1544152302783" POSITION="left" TEXT="Agenda">
+<node CREATED="1544152303580" ID="ID_458579958" MODIFIED="1544152326327" TEXT="K&#x1ec3; v&#x1ec1; &#x1ee9;ng d&#x1ee5;ng c&#x1ee7;a signal trong d&#x1ef1; &#xe1;n tr&#x1b0;&#x1edb;c kia"/>
+<node CREATED="1544152339194" ID="ID_1635440525" MODIFIED="1544152382280" TEXT="C&#xf3; nhi&#x1ec1;u l&#x1ed7;i x&#x1ea3;y ra li&#xea;n quan &#x111;&#x1ebf;n signal"/>
+</node>
+<node CREATED="1544152554347" ID="ID_101401306" MODIFIED="1544152555846" POSITION="left" TEXT="T&#x1ed5;ng quan v&#x1ec1; signal">
+<node CREATED="1544152556747" ID="ID_231687838" MODIFIED="1544152575559" TEXT="Trong d&#x1ef1; &#xe1;n h&#x1ed3;i x&#x1b0;a t&#x1eeb;ng th&#x1eed; ignore kill signal nh&#x1b0;ng ko &#x111;&#x1b0;&#x1ee3;c"/>
+</node>
+<node CREATED="1544152781011" ID="ID_1561069040" MODIFIED="1544152782367" POSITION="left" TEXT="X&#x1eed; l&#xfd; v&#x1edb;i signal">
+<node CREATED="1544152783315" ID="ID_1547478941" MODIFIED="1544152791791" TEXT="V&#xed; d&#x1ee5; code &#x111;&#x1ec3; x&#x1eed; l&#xfd; 1 signal"/>
+<node CREATED="1544154366673" ID="ID_1653811301" MODIFIED="1544154383045" TEXT="K&#x1ec3; v&#x1ec1; bug li&#xea;n quan &#x111;&#x1ebf;n vi&#x1ec7;c b&#x1ea5;t &#x111;&#x1ed3;ng b&#x1ed9; khi s&#x1eed; d&#x1ee5;ng signal"/>
+</node>
+<node CREATED="1544156545112" ID="ID_1106484620" MODIFIED="1544156545829" POSITION="left" TEXT="Nh&#x1eef;ng function an to&#xe0;n khi g&#x1ecd;i trong signal handler">
+<node CREATED="1544156548271" ID="ID_1928665789" MODIFIED="1544156557709" TEXT="Ch&#xe8;n th&#xea;m h&#xec;nh &#x1ea3;nh c&#xe1;c safe function"/>
+</node>
+<node CREATED="1544163492277" ID="ID_1450279294" MODIFIED="1544168688144" POSITION="right" TEXT="Mask, unmask, block, unblock a signal">
+<node CREATED="1544163533446" ID="ID_932156650" MODIFIED="1544163587137" TEXT="Process can mask signal n&#xe0;o n&#xf3; mu&#x1ed1;n nh&#x1ead;n, ch&#x1ec9; c&#xf3; signal c&#xf3; c&#xf9;ng mask m&#x1edb;i &#x111;&#x1b0;&#x1ee3;c kernel g&#x1eed;i &#x111;&#x1ebf;n cho process"/>
+<node CREATED="1544163589117" ID="ID_631348507" MODIFIED="1544163642345" TEXT="&#x110;&#xf4;i khi trong 1 th&#x1edd;i &#x111;i&#x1ec3;m, process kh&#xf4;ng mu&#x1ed1;n x&#x1eed; l&#xfd; 1 s&#x1ed1; signal n&#xe0;o &#x111;&#xf3;, n&#xf3; c&#xf3; th&#x1ec3; block signal &#x111;&#xf3;. Signal v&#x1eab;n &#x111;&#x1b0;&#x1ee3;c g&#x1eed;i &#x111;&#x1ebf;n cho process nh&#x1b0;ng s&#x1ebd; n&#x1eb1;m trong h&#xe0;ng ch&#x1edd;, ko &#x111;&#x1b0;&#x1ee3;c x&#x1eed; l&#xfd; ngay."/>
+</node>
+<node CREATED="1544164930043" ID="ID_335909046" MODIFIED="1544165714926" POSITION="right" TEXT="Initialize a sigset">
+<node CREATED="1544165121161" ID="ID_339657008" MODIFIED="1544165121829" TEXT="int sigfillset(sigset_t *set)"/>
+<node CREATED="1544164937500" ID="ID_318413493" MODIFIED="1544164944628" TEXT="int sigaddset(sigset_t *set, int signo)"/>
+<node CREATED="1544165145520" ID="ID_1519656565" MODIFIED="1544165146332" TEXT="int sigdelset(sigset_t *set, int signo)"/>
+<node CREATED="1544164971163" ID="ID_1205893786" MODIFIED="1544164983930" TEXT="int sigismember(const sigset_t *set, int signo)"/>
+</node>
+<node CREATED="1544165715772" ID="ID_683806079" MODIFIED="1544165739396" POSITION="right" TEXT="Mask, unmask a set of signal">
+<node CREATED="1544165751043" ID="ID_1839612711" MODIFIED="1544165933223" TEXT=" int sigprocmask(int how, const sigset_t *set, sigset_t *oldset)"/>
+<node CREATED="1544166057451" ID="ID_1179625098" MODIFIED="1544166058263" TEXT="SIG_BLOCK"/>
+<node CREATED="1544166066555" ID="ID_689734924" MODIFIED="1544166067015" TEXT="SIG_UNBLOCK"/>
+<node CREATED="1544166072235" ID="ID_1897970849" MODIFIED="1544166073302" TEXT="SIG_SETMASK"/>
+<node CREATED="1544166076315" ID="ID_106552119" MODIFIED="1544166081687" TEXT="Th&#x1ef1;c h&#xe0;nh">
+<node CREATED="1544166082531" ID="ID_392630826" MODIFIED="1544166112593" TEXT="Vi&#x1ebf;t 1 ch&#x1b0;&#x1a1;ng tr&#xec;nh C &#x111;&#x1ec3; unset ho&#x1eb7;c block Ctrl + C signal t&#x1eeb; user"/>
+<node CREATED="1544166200900" ID="ID_527256449" MODIFIED="1544166234088" TEXT="Vi&#x1ebf;t 1 ch&#x1b0;&#x1a1;ng tr&#xec;nh C &#x111;&#x1ec3; in ra c&#xe1;c signal mask c&#x1ee7;a process hi&#x1ec7;n t&#x1ea1;i"/>
+</node>
+</node>
+<node CREATED="1544166460757" ID="ID_518845664" MODIFIED="1544166502610" POSITION="right" TEXT="Ki&#x1ec3;m tra c&#xe1;c signal &#x111;ang b&#x1ecb; pending">
+<node CREATED="1544166494172" ID="ID_173338449" MODIFIED="1544166494697" TEXT="int sigpending(sigset_t *set)"/>
+<node CREATED="1544166592821" ID="ID_360611070" MODIFIED="1544166638129" TEXT="M&#x1ed9;t signal &#x111;ang b&#x1ecb; block th&#xec; kernel v&#x1eab;n g&#x1eed;i n&#xf3; &#x111;&#x1ebf;n cho process nh&#x1b0;ng signal &#x111;&#xf3; s&#x1ebd; b&#x1ecb; pending trong h&#xe0;ng &#x111;&#x1ee3;i m&#xe0; kh&#xf4;ng &#x111;&#x1b0;&#x1ee3;c x&#x1eed; l&#xfd;."/>
+<node CREATED="1544166641121" ID="ID_157562330" MODIFIED="1544166713530" TEXT="Th&#x1ef1;c h&#xe0;nh">
+<node CREATED="1544166717660" ID="ID_1629695012" MODIFIED="1544166723337" TEXT="Vi&#x1ebf;t 1 ch&#x1b0;&#x1a1;ng tr&#xec;nh C block SIGINT, sau &#x111;&#xf3; check trong s&#x1ed1; signal pending c&#xf3; SIGINT kh&#xf4;ng, n&#x1ebf;u c&#xf3; th&#xec; tho&#xe1;t ch&#x1b0;&#x1a1;ng tr&#xec;nh"/>
+</node>
+</node>
+<node CREATED="1544167038014" ID="ID_232058518" MODIFIED="1544169005670" POSITION="right" TEXT="Th&#x1ef1;c h&#xe0;nh">
+<node CREATED="1544167047539" ID="ID_1095842247" MODIFIED="1544167256896" TEXT="Vi&#x1ebf;t ch&#x1b0;&#x1a1;ng tr&#xec;nh A fork ra ch&#x1b0;&#x1a1;ng tr&#xec;nh B. A v&#xe0; B c&#xf9;ng ghi v&#xe0;o 1 file. M&#x1ed7;i ch&#x1b0;&#x1a1;ng tr&#xec;nh s&#x1ebd; &#x111;&#x1ecd;c l&#x1ea7;n l&#x1b0;&#x1ee3;t d&#xf2;ng counter cu&#x1ed1;i c&#xf9;ng c&#x1ee7;a file v&#xe0; t&#x103;ng n&#xf3; l&#xea;n 1, sau &#x111;&#xf3; ghi ti&#x1ebf;p 1 d&#xf2;ng m&#x1edb;i l&#xe0; process ID c&#x1ee7;a ch&#xed;nh n&#xf3; k&#xe8;m bi&#x1ebf;n counter m&#x1edb;i. Ph&#x1ea3;i s&#x1eed; d&#x1ee5;ng signal &#x111;&#x1ec3; &#x111;&#x1ed3;ng b&#x1ed9; gi&#x1eef;a 2 process"/>
+</node>
+</node>
+</map>
