b01fdce890ed556e4bca0bdbbd9ab12834abb95f
LinuxEmbeddedPractive <linuxembeddedpractive@gmail.com>
2019-05-06 15:08:30 +0700

    Create unit 18
    Create unit 18

---
+++ Bai 18/Watchdog_driver.mm
@@ -0,0 +1,41 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1555049518292" ID="ID_1880193722" MODIFIED="1555049581079" TEXT="Watchdog driver">
+<node CREATED="1555051524136" ID="ID_1956213047" MODIFIED="1555051527845" POSITION="right" TEXT="Agenda">
+<node CREATED="1555051538456" ID="ID_1753472305" MODIFIED="1555051539065" TEXT="Overview"/>
+<node CREATED="1555051543064" ID="ID_1012600999" MODIFIED="1555051543764" TEXT="Watchdog operation"/>
+<node CREATED="1555051547184" ID="ID_1637770022" MODIFIED="1555051547589" TEXT="Watchdog standard for Linux"/>
+<node CREATED="1555051551096" ID="ID_714787484" MODIFIED="1555051552059" TEXT="Explain a watchdog driver"/>
+</node>
+<node CREATED="1555049582159" ID="ID_924601128" MODIFIED="1555049587180" POSITION="right" TEXT="Overview">
+<node CREATED="1555049587999" ID="ID_1284260950" MODIFIED="1555051847748" TEXT="&#x110;&#xf4;i khi do l&#x1ed7;i x&#x1ea3;y ra, h&#x1ec7; th&#x1ed1;ng r&#x1a1;i v&#xe0;o tr&#x1ea1;ng th&#xe1;i treo c&#x1ee9;ng v&#xe0; kh&#xf4;ng th&#x1ec3; t&#x1ef1; tho&#xe1;t ra &#x111;&#x1b0;&#x1ee3;c. L&#xfa;c &#x111;&#xf3;, h&#x1ec7; th&#x1ed1;ng c&#x1ea7;n m&#x1ed9;t thi&#x1ebf;t b&#x1ecb; c&#xf3; th&#x1ec3; &#x1ea5;n &#x111;&#x1b0;&#x1ee3;c n&#xfa;t reset. V&#xe0; watchdog &#x111;&#x1b0;&#x1ee3;c sinh ra &#x111;&#x1ec3; l&#xe0;m vi&#x1ec7;c &#x111;&#xf3;. "/>
+</node>
+<node CREATED="1555049826784" ID="ID_746617347" MODIFIED="1555049832285" POSITION="right" TEXT="Watchdog operation"/>
+<node CREATED="1555049943625" ID="ID_97532428" MODIFIED="1555049965501" POSITION="right" TEXT="Watchdog standard for Linux">
+<node CREATED="1555050477365" ID="ID_1188668176" MODIFIED="1555050482250" TEXT="Documentation">
+<node CREATED="1555050532104" ID="ID_1957780973" MODIFIED="1555050604309" TEXT="/Documentation/watchdog"/>
+</node>
+</node>
+<node CREATED="1555050127697" ID="ID_119128899" MODIFIED="1555050138813" POSITION="right" TEXT="Watchdog config rules"/>
+<node CREATED="1555050155392" ID="ID_400004401" MODIFIED="1555050177750" POSITION="right" TEXT="Kernel API for watchdog">
+<node CREATED="1555050570832" ID="ID_152542958" MODIFIED="1555053683391" TEXT="struct watchdog_ops">
+<node CREATED="1555050592168" ID="ID_140310752" MODIFIED="1555050594109" TEXT="/include/linux/watchdog.h"/>
+</node>
+</node>
+<node CREATED="1555050901320" ID="ID_520917074" MODIFIED="1555050931341" POSITION="right" TEXT="Explain a watchdog driver">
+<node CREATED="1555051127553" ID="ID_1884528499" MODIFIED="1555051130485" TEXT="Device tree"/>
+<node CREATED="1555051131816" ID="ID_1620684865" MODIFIED="1555051138181" TEXT="Probe function"/>
+<node CREATED="1555051207649" ID="ID_368643680" MODIFIED="1555051217029" TEXT="watchdog shutdown"/>
+<node CREATED="1555051241024" ID="ID_1608066334" MODIFIED="1555051254955" TEXT="watchdog suspend and resume"/>
+<node CREATED="1555051292788" ID="ID_1131988703" MODIFIED="1555054025278" TEXT="watchdog device file operation">
+<node CREATED="1555051303704" ID="ID_155840879" MODIFIED="1555051305845" TEXT="start"/>
+<node CREATED="1555051306344" ID="ID_340614762" MODIFIED="1555051308668" TEXT="ping"/>
+<node CREATED="1555051309456" ID="ID_788426976" MODIFIED="1555051316213" TEXT="set_timeout"/>
+<node CREATED="1555051317136" ID="ID_301735896" MODIFIED="1555051320969" TEXT="restart"/>
+</node>
+</node>
+<node CREATED="1555051356368" ID="ID_743819150" MODIFIED="1555051360412" POSITION="right" TEXT="Homework">
+<node CREATED="1555051361336" ID="ID_363015042" MODIFIED="1555051379283" TEXT="Vi&#x1ebf;t watchdog driver theo chu&#x1ea9;n Linux cho board m&#xe0; b&#x1ea1;n &#x111;ang d&#xf9;ng"/>
+</node>
+</node>
+</map>
