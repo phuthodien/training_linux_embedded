8077d27c8b4ddcb3d2aee794eecccf09ac911e45
LinuxEmbeddedPractive <linuxembeddedpractive@gmail.com>
2019-05-06 15:07:11 +0700

    Create Unit 14
    Create Unit 14

---
+++ Bai 14/Virtual memory management.mm
@@ -0,0 +1,25 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1551409205211" ID="ID_716189947" MODIFIED="1551421408874" TEXT="Virtual memory management">
+<node CREATED="1551409744241" ID="ID_1992950710" MODIFIED="1551409755582" POSITION="right" TEXT="M&#x1ed9;t v&#xed; d&#x1ee5; v&#x1ec1; s&#x1ef1; c&#x1ea7;n thi&#x1ebf;t c&#x1ee7;a virtual memory"/>
+<node CREATED="1551409883498" ID="ID_1531417809" MODIFIED="1551409890345" POSITION="right" TEXT="Page frame">
+<node CREATED="1551409915890" ID="ID_1359659738" MODIFIED="1551409916950" TEXT="Page Descriptors"/>
+</node>
+<node CREATED="1551410011169" ID="ID_1523241392" MODIFIED="1551421650922" POSITION="right" TEXT="Virtual and physical memory mapping">
+<node CREATED="1551410097657" ID="ID_1486948011" MODIFIED="1551410104838" TEXT="Process page table"/>
+<node CREATED="1551410145009" ID="ID_1924515018" MODIFIED="1551410160925" TEXT="Virtual address, logical address, physical address"/>
+</node>
+<node CREATED="1551421284989" ID="ID_1626516955" MODIFIED="1551421312650" POSITION="right" TEXT="Page fault exception"/>
+<node CREATED="1551410288537" ID="ID_450425737" MODIFIED="1551410353230" POSITION="right" TEXT="Memory access in virtual mode and protected mode"/>
+<node CREATED="1551410368602" ID="ID_299048483" MODIFIED="1551421685523" POSITION="right" TEXT="Cached memory">
+<node CREATED="1551411976805" ID="ID_1475423931" MODIFIED="1551412016187" TEXT="Some use cases need to prevent cached memory"/>
+</node>
+<node CREATED="1551420990149" ID="ID_254634615" MODIFIED="1551420997073" POSITION="right" TEXT="Memory allocator">
+<node CREATED="1551421004173" ID="ID_1474913048" MODIFIED="1551421027610" TEXT="Non continuous allocator">
+<node CREATED="1551421078157" ID="ID_1089527846" MODIFIED="1551421087594" TEXT="Lazy allocate"/>
+</node>
+<node CREATED="1551421043685" ID="ID_432224207" MODIFIED="1551421049634" TEXT="Slab allocator"/>
+</node>
+<node CREATED="1551421179405" ID="ID_1474883462" MODIFIED="1551421189976" POSITION="right" TEXT="Memory cleanup"/>
+</node>
+</map>
